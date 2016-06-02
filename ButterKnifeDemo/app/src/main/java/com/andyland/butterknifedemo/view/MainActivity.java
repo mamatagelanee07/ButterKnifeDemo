b81689dfeaa58709daa7aa7c5ddc3c0a0d8d7767
mamatagelanee07 <gelaneem@gmail.com>
2016-06-02 10:31:48 +0530

    Added crashanalytics

--- ButterKnifeDemo/app/src/main/java/com/andyland/butterknifedemo/view/MainActivity.java
+++ ButterKnifeDemo/app/src/main/java/com/andyland/butterknifedemo/view/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         // Added Crashnalytic to track crashes in demo
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
